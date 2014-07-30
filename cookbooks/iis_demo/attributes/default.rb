df7f979e7942725bc09d9a974481aabdf7e4321a
Josh Rolstad <jrolstad@gmail.com>
2014-07-30 10:45:24 -0700

    Updating iis_demo cookbook to create multiple sites per port

--- cookbooks/iis_demo/attributes/default.rb
+++ cookbooks/iis_demo/attributes/default.rb
@@ -1,1 +1,3 @@
 default["iis_demo"]["indexfile"] = "default2.htm"
+default["iis_demo"]["sites"]["clowns"] = {"port" => 80}
+default["iis_demo"]["sites"]["bears"] = {"port" => 81}
--- cookbooks/iis_demo/recipes/default.rb
+++ cookbooks/iis_demo/recipes/default.rb
@@ -1,13 +1,47 @@
 powershell_script "Install IIS" do
-  action:run
   code "add-windowsfeature Web-Server"
+  action :run
 end
 
 service "w3svc" do
-  action [:enable, :start]
+  action [:enable, :start ]
 end
 
-cookbook_file 'c:\inetpub\wwwroot\default.htm' do
-  source node["iis_demo"]["indexfile"]
-  rights :read, "Everyone"
+#node.default["iis_demo"]["indexfile"] = "Default2.htm"
+#cookbook_file "c:\\inetpub\\wwwroot\\Default.htm" do
+#  source node["iis_demo"]["indexfile"]
+#  rights :read, "Everyone"
+#end
+
+powershell_script "disable default site" do
+    code 'get-website "Default Web Site*" | where {$_.state -ne "Stopped"} | Stop-Website'
+end
+
+node['iis_demo']['sites'].each do |site_name, site_data|
+    site_dir = "#{ENV['SYSTEMDRIVE']}\\inetpub\\wwwroot\\#{site_name}"
+    directory site_dir
+
+    powershell_script "create app pool for #{site_name}" do
+        code "New-WebAppPool #{site_name}"
+        not_if "C:\\Windows\\System32\\inetsrv\\appcmd.exe list apppool #{site_name}"
+    end
+
+    powershell_script "new website for #{site_name}" do
+        code <<-EOH
+            Import-Module webadministration
+            if(-not(test-path IIS:\\Sites\\#{site_name})){
+                New-WebSite -name #{site_name} -Port #{site_data['port']} -PhysicalPath #{site_dir} -ApplicationPool #{site_name}
+            }
+        EOH
+    end
+
+    template "#{site_dir}\\default.htm" do
+        source "default.htm.erb"
+        rights :read, "Everyone"
+        variables(
+            :site_name => site_name,
+            :port => site_data['port']
+            )
+        notifies :restart, "service[w3svc]"
+    end
 end
