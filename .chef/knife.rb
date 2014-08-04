d1a4e0ca748fc0dfab283f21857df8beffdcd517
Josh Rolstad <jrolstad@gmail.com>
2014-08-04 02:26:17 +0000

    Updating the knife configuration back to standard

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,3 +1,5 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
