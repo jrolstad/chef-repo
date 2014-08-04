f2a48754607040bfd9eee59e91193e95da2a6299
Josh Rolstad <jrolstad@gmail.com>
2014-08-04 04:15:33 +0000

    Updating chef workstation role

--- roles/chef_workstation_windows.rb
+++ roles/chef_workstation_windows.rb
@@ -1,3 +1,3 @@
 name "chef_workstation_windows"
 description "Chef workstation on a windows machine"
-run_list "recipe[powershell]", "recipe[chocolatey]"
+run_list "recipe[powershell]", "recipe[chocolatey]", "recipe[msdotnet45]","recipe[developer_tools::git]","recipe[developer_tools::tortoise_git]","recipe[developer_tools::chocolatey]","recipe[developer_tools::atom]","recipe[developer_tools::google_chrome]","recipe[developer_tools::knife_windows]","recipe[developer_tools::knife_ec2]"
